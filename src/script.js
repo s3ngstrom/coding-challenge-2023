021fc7d7e24b7c1b4fb92b1aa2ac44fd87f17f3f
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-10-09 03:17:39 -0500

    Updated meta tags, comment cleanup, smaller media

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+/* Party size should reasonably have some limit, based on the restaurant's needs. 
+Set to 20 People in this script, but restaurant may want users to call for reservations greater than 10+ people, for example. */
 function generatePartySizeOptions() {
   const selectPartySize = document.getElementById("party-size");
   for (let i = 1; i <= 20; i++) {
@@ -6,9 +8,9 @@ function generatePartySizeOptions() {
     selectPartySize.appendChild(option);
   }
 }
-
 generatePartySizeOptions();
 
+/* IF it's only one person, preventing "1 People" being displayed */
 const partySizeDropdown = document.getElementById("party-size");
 partySizeDropdown.addEventListener("change", function () {
   const selectedValue = partySizeDropdown.value;
@@ -21,9 +23,11 @@ partySizeDropdown.addEventListener("change", function () {
   partySizeDropdown.options[partySizeDropdown.selectedIndex].text = newText;
 });
 
+/* This may be unnecessary IF we can access a better time input field via vanilla HTML.
+Accepts reservations from 10 AM to 11:30 PM; assuming that restaurant may not necessarily accept reservations a certain amount of time before closing.*/
 function generateTimeOptions() {
   const selectTime = document.getElementById("reservation-time");
-  for (let hour = 11; hour <= 23; hour++) {
+  for (let hour = 10; hour <= 23; hour++) {
     for (let minutes = 0; minutes < 60; minutes += 30) {
       const time = `${hour}:${minutes === 0 ? "00" : minutes}`;
       const formattedHour = hour > 12 ? hour - 12 : hour;
@@ -36,9 +40,10 @@ function generateTimeOptions() {
     }
   }
 }
-
 generateTimeOptions();
 
+
+/* Reservation form listener and handler*/
 document.addEventListener("DOMContentLoaded", function () {
   const reservationForm = document.getElementById("reservation-form");
   const customerInfoForm = document.getElementById("customer-info-form");
@@ -73,18 +78,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  /* Validate date */
   function isValidCalendarDate(date) {
     const currentDate = new Date();
     currentDate.setHours(0, 0, 0, 0);
-    return (
-      date instanceof Date && !isNaN(date) && date >= currentDate 
-    );
+    return date instanceof Date && !isNaN(date) && date >= currentDate;
   }
 
+  /* Validate time */
   function isValidReservationTime(time) {
     return time !== "";
   }
 
+  /* Validate party size */
   function isValidPartySizeSelection(partySize) {
     return partySize !== "";
   }
